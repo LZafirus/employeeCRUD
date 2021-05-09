2bda24aa36a4ad69e6a1849daf752eaf62344d8b
owca <walaszczyk.dawid@gmail.com>
2021-05-09 21:01:11 +0200

    Basic methods

--- src/main/java/com/walaszczyk/example/employeeCRUD/rest/EmployeeRestController.java
+++ src/main/java/com/walaszczyk/example/employeeCRUD/rest/EmployeeRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +29,21 @@ public class EmployeeRestController {
 		return employeeService.findAll();
 	}
 	
+	@GetMapping("/employees/{id}")
+	public Employee getEmployee(@PathVariable int id) {
+		Employee employee = employeeService.findById(id);
+		if (employee == null ) {
+			throw new RuntimeException("Employee not found - "+id );			
+		}
+		return employee;
+	}
+	
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		employee.setId(0);
+		
+		employeeService.save(employee);
+		
+		return employee;
+	}
 }
