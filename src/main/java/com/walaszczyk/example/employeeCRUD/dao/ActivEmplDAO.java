a9c30ee73f1d5697d0841c10d25f07cf20a2532c
owca <walaszczyk.dawid@gmail.com>
2021-05-10 21:32:15 +0200

    Clearing code. Project done.

--- src/main/java/com/walaszczyk/example/employeeCRUD/dao/ActivEmplDAO.java
+++ src/main/java/com/walaszczyk/example/employeeCRUD/dao/ActivEmplDAO.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.walaszczyk.example.employeeCRUD.entity.ActiveEmployee;
-import com.walaszczyk.example.employeeCRUD.entity.Team;
 
 @RepositoryRestResource(path="activeEmployees")
 public interface ActivEmplDAO extends JpaRepository<ActiveEmployee, Integer> {
